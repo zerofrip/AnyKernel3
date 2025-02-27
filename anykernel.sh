97a1157f4d6eb125b08151c06fefe20ad0b3470a
zerofrip <zerofripside@gmail.com>
2025-02-27 10:02:36 +0900

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Unofficial LineageOS Spacewar Kernel
+kernel.string=Unofficial Spacewar LineageOS Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=1
