92c99ee0c27e519216aea2ec9d6a3aef7aa6490d
zerofrip <zerofripside@gmail.com>
2025-02-27 10:03:06 +0900

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Unofficial NOS3.0 Spacewar Kernel
+kernel.string=Unofficial Spacewar NOS3.0 Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=1
